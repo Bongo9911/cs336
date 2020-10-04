9cc60c5f9edd08ba4cb9b094f3fe529ba6143573
Bongo9911 <dawsonbuist@gmail.com>
2020-10-03 20:28:55 -0400

    added comments to JS

--- hmwk4/hmwk4.js
+++ hmwk4/hmwk4.js
@@ -1,11 +1,11 @@
 function keyUpHandler() {
     tb = document.getElementById("textbox");
-    tb.value = tb.value.toUpperCase();
+    tb.value = tb.value.toUpperCase(); //converts all characters to uppercase
 
     words = document.getElementById("words");
-    matches = tb.value.match(/[\w\d\’\'-]+/gi);
+    matches = tb.value.match(/[\w\d\’\'-]+/gi); //finds words by checking for non-space characters then a space
     numWords = matches ? matches.length : 0
-    words.innerHTML = numWords + (numWords != 1 ? " words" : " word");
+    words.innerHTML = numWords + (numWords != 1 ? " words" : " word"); //uses plural when needed
 
     characters = document.getElementById("characters");
     characters.innerHTML = tb.value.length + (tb.value.length != 1 ? " characters" : " character");
