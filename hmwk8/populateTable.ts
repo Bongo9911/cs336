a450a2817d0d4eb58b45c71f68bb4f86728cdb61
Bongo9911 <dawsonbuist@gmail.com>
2020-10-24 16:02:57 -0400

    Completed Homework 8

---
+++ hmwk8/populateTable.ts
@@ -0,0 +1,34 @@
+import DataService, { Person } from './dataService';
+
+"use strict";
+
+//gets the data for the rows from the DataService class and calls createRow
+//on all of the returned data
+async function addDataRows() {
+    let table : HTMLTableElement = <HTMLTableElement> document.getElementById("myTable");
+    const ds : DataService = new DataService();
+    await ds.fetchData();
+    let data : Person[] = ds.getData();
+    data.forEach(element => { //loops through the array to get row data
+        createRow(table, element); //passes data for each row into createRow
+    });
+}
+
+//creates a row with data provieded in rowData and adds it to the DOM under the
+//parentElem
+function createRow(parentElem: HTMLTableElement, rowData : Person) {
+    let row = parentElem.insertRow(-1); //creates a row at the end of the table
+
+    //Adds data to the row
+    row.insertCell(0).innerHTML = rowData.name.title + " " + rowData.name.first + " " + rowData.name.last;
+    row.insertCell(1).innerHTML = rowData.gender;
+    row.insertCell(2).innerHTML = rowData.location.street.number + " " + rowData.location.street.name
+        + ", " + rowData.location.city + ", " + rowData.location.state + ", " + rowData.location.country;
+    row.insertCell(3).innerHTML = rowData.dob.age.toString();
+    row.insertCell(4).innerHTML = rowData.phone;
+
+    //Create a cell to put the image in and then append the image to the cell
+    let imgCell = row.insertCell(5);
+    var img = document.createElement('img');
+    img.src = rowData.picture.medium;    imgCell.appendChild(img);
+}
